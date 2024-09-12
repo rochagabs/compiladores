b9d0b09b47bb7b20691358d4fbd8dc702b639b68
joseadelson2002 <joseadelson2002@academico.ufs.br>
2024-09-12 12:41:35 -0300

    modifica main para permitir impressão de árvore AST

--- avaliacao2/main.java
+++ avaliacao2/main.java
@@ -1,31 +1,32 @@
-package caju;
-
-import caju.lexer.*;
-import caju.parser.*;
-import caju.node.*;
+package calculadora;
+import calculadora.parser.*;
+import calculadora.lexer.*;
+import calculadora.node.*;
 import java.io.*;
 
 public class Main
 {
-	public static void main(String[] args)
-	{
-		try
-		{
-			String arquivo = "teste/teste.calc";
-
-			Lexer lexer =
-					new Lexer(
-							new PushbackReader(  
-									new FileReader(arquivo), 1024)); 
-			
-			Parser parser = new Parser(lexer);
-			Start tree = parser.parse();
-			System.out.println(tree);
-			
-		}
-		catch(Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
+ public static void main(String[] args)
+ {
+  try
+  {
+   String arquivo = "teste/test.calc";
+  
+   Lexer lex = new Lexer(
+		    new PushbackReader(  
+		    new FileReader(arquivo), 1024));
+   
+   Parser p = new Parser(lex); 
+   
+   Start tree = p.parse();
+   //Imprime árvore na saída padrão
+   //tree.apply(new ASTPrinter());
+   //Imprime árvore em interface gráfica
+   tree.apply(new ASTDisplay());
+  }
+  catch(Exception e)
+  {
+   System.out.println(e.getMessage());
+  }
+ }
 }
